22b87ef8c3ae6f3e92686f46636bd6d96cc4aa02
Ale962 <isono@hotmail.it>
2025-02-18 10:12:56 +0100

    Exercise Lesson 2

---
+++ Lesson_2/Ptyhon_Exercise_Data_Types_Operators_And Collections/# Lesson_2_Exercise_2.4.py
@@ -0,0 +1,9 @@
+# Lesson 2 Exercise 2.4
+
+'''2-4. Name Cases: Use a variable to represent a person’s name, and then print that person’s name in lowercase, uppercase, and title case.'''
+
+name2: str = "Racheel"
+
+print (name2.lower())
+print (name2.upper())
+print (name2.title())
---
+++ Lesson_2/Ptyhon_Exercise_Data_Types_Operators_And Collections/# Lesson_2_Exercise_2.5-6.py
@@ -0,0 +1,7 @@
+# Lesson 2 Exercise 2.5-6
+
+'''2-5. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks: Albert Einstein once said, “A person who never made a mistake never tried anything new.”'''
+
+famousperson: str = "Giulio Cesare"
+
+print (f"{famousperson} once said, \"Alea iacta est\"")
