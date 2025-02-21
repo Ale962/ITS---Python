430b64c146dad585620c067104f62b66575f1145
Ale962 <isono@hotmail.it>
2025-02-21 13:08:02 +0100

    Exercise 5.10 correction

--- Lesson_3/Python_Exercise/# Lesson_3_Exercise_5.10.py
+++ Lesson_3/Python_Exercise/# Lesson_3_Exercise_5.10.py
@@ -11,17 +11,28 @@
 current_user: list[str] = ["Alessio", "Diego", "Rachele", "Rebecca", "Riccardo"]
 new_users: list[str] = ["Diego", "Riccardo", "Claudio", "Domenico", "Luca"]
 
-not_usable_names_1 = [ name.lower() for name in current_user ]
-not_usable_names_2 = [ name.upper() for name in current_user ]
+not_usable_names_1: list[str] = [ name.lower() for name in current_user ]
+not_usable_names_2: list[str] = [ name.upper() for name in current_user ]
+not_usable_names_3: list[str] = [ name.lower() for name in new_users ]
+not_usable_names_4: list[str] = [ name.upper() for name in new_users ]
+used_user_names: list[str] = not_usable_names_1 + not_usable_names_2 + not_usable_names_3 + not_usable_names_4
 
 user: str = str(input("Put your user name here: "))
 
-if user in current_user or new_users:
+if user in current_user or user in new_users:
     print(f"Welcome back {user}, thank you for logging in today")
 
 else:
-    if user == new_users or not_usable_names_1 or not_usable_names_2:
+
+    if user in current_user or user in new_users or user.lower() in used_user_names or user.upper() in used_user_names:
         print("User name already taken, please restart the process and insert a new name")
+
     else:
-        new_users.append(user)
-        print(f"Hello {user}, thank you for joining us")
+        confirmation: str = str(input("Do you want to confirm new name user? Y/N: "))
+        
+        if confirmation == "Y":
+            new_users.append(user)
+            print(f"Hello {user}, thank you for joining us")
+        
+        else:
+            print("Please restart the registration")
